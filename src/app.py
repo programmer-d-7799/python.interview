523530fb1992bb7840ba12df6cf2a2256425f15c
D-R <damp.rain.ad@gmail.com>
2024-03-19 11:32:35 +1300

    complete

---
+++ src/app.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+
+def example_method():
+    return True
+
+
+if __name__ == "__main__":
+    print("Hello world")
+    example_method()
---
+++ src/test_app.py
@@ -0,0 +1,13 @@
+import pytest
+from app import *
+
+
+def test_example_method():
+    # arrange
+    expected = True
+
+    # action
+    result = example_method()
+
+    # assert
+    assert result == expected
